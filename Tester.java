12ff968fdeffba8e55303aa400237e9e25288fa8
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2023-12-31 14:10:38 +0200

    עודכן

--- Tester.java
+++ Tester.java
@@ -28,11 +28,25 @@ public class Tester {
         }
 
     }
+static int count = 0; //משתנה עבור ex4
+public static int ex4(int num)
+{
+    if(num > 0)
+    {
+        {
+            count++;
+            ex4(num /= 10);
+        }
+        return count;
+    }
+    return 0;
+}   
 
     public static void main(String[] args)
     {
 //        ex1(5);
 //        ex2(4);
 //        System.out.println((ex3(5, 3)));
+//        System.out.println(ex4(7864));
     }
 }
